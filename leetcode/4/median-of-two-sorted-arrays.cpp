ab4a8d1355ef5756449b4ba7ef6c55993a809a99
anosnowhong <anosnowhong@gmail.com>
2016-10-20 03:14:28 +0100

    ugly solution

--- leetcode/4/median-of-two-sorted-arrays.cpp
+++ leetcode/4/median-of-two-sorted-arrays.cpp
@@ -1,19 +1,81 @@
+/*
+  Half of merge sort
+  Watch out blank input
+  
+*/
 class Solution {
 public:
   double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
     //rearrange
     vector<int> tmp;
     double result;
-    tmp.insert(tmp.end(), nums1.begin(), nums1.end());
-    tmp.insert(tmp.end(), nums2.begin(), nums2.end());
 
-    sort(tmp.begin(),tmp.end());
-
-    if(tmp.size()%2 != 0){
-      result = tmp[tmp.size()/2];
-    }else{
-      result = (tmp[tmp.size()/2] + tmp[tmp.size()/2 - 1])/2.0;
+    int len_total = nums1.size()+nums2.size();
+    int half_search = len_total/2 + 1;
+    int j = 0, k = 0;
+   
+    while(half_search>0){
+      if(nums1.size()<nums2.size()){
+          
+        if(j<nums1.size()){
+          if(nums1[j]<nums2[k]){
+            tmp.push_back(nums1[j]);
+            j++;
+            half_search--;
+          }
+          else{
+            tmp.push_back(nums2[k]);
+            k++;
+            half_search--;
+          }
+        }
+        else{
+            tmp.push_back(nums2[k]);
+            k++;
+            half_search--;
+        }
+        
+      }
+      else{
+          if(j<nums2.size()){
+            if(k==nums1.size()){
+              tmp.push_back(nums2[j]);
+              j++;
+              half_search--;
+            }
+            else{
+                if(nums2[j]<nums1[k]){
+              tmp.push_back(nums2[j]);
+              j++;
+              half_search--;
+            }
+            else{
+              tmp.push_back(nums1[k]);
+              k++;
+              half_search--;
+            }
+            }
+            
+          }
+           else{
+            tmp.push_back(nums1[k]);
+            k++;
+            half_search--;
+        }
+  
+        }
+      
     }
-    return result;
-  }
-};
+
+      if(len_total%2 != 0){
+        result = tmp[tmp.size()-1];
+      }else{
+        result = (tmp[tmp.size()-2] + tmp[tmp.size()-1])/2.0;
+      }
+      cout<<tmp[0]<<endl;
+      cout<< tmp[1]<<endl;
+      cout<< tmp[2]<<endl;
+      
+      return result;
+    }
+  };
