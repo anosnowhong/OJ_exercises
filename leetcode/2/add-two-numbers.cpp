6bf593b17c6b51e1531220de5aacdcc7b5d6787b
anosnowhong <anosnowhong@gmail.com>
2016-10-31 19:11:42 +0000

    clean

--- leetcode/2/add-two-numbers.cpp
+++ leetcode/2/add-two-numbers.cpp
@@ -7,62 +7,50 @@
  * };
  */
 class Solution {
-public:
-  typedef struct ListNode *PtrtoNode;
-  typedef PtrtoNode List;
-  typedef PtrtoNode Position;
+	public:
+		ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+		    ListNode* result = new ListNode(0);
+		    ListNode* rpoint;
+		    int sum;
 
-  //by simply print out the list content you can find that this singly-linked list is implemented without a header
-  ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-    Position tmp1, tmp2,pnode, sum_node;
-    List result;
+		    if(l1==NULL&&l2!=NULL) return l2;
+		    if(l2==NULL&&l1!=NULL) return l1;
+		    if(l1==NULL&&l2==NULL) return result;
 
-    tmp1 = l1;
-    tmp2 = l2;
-    pnode = result;
-    int up = 0, sum;
-    //be careful to special input case such as [x,x,5]\n[x,x,5] at the end of each input the sum exceed 10
-    while(true){
-      //need check if malloc failed
-      sum_node = malloc(sizeof(struct ListNode));
-      if(tmp1->Next == NULL){
-        if(tmp2->Next == NULL){
-          break;
-        }
-        sum = tmp2->val;
-      }
-      else if(tmp2->Next == NULL){
-        sum = tmp1->val;
-      }
-      else{
-        sum = tmp1->val + tmp2->val;
-      }
+		    sum = l1->val+l2->val;
+		    result->val = sum%10;
+		    sum /= 10;
+		    rpoint = result;
 
-      sum += up;
-      if(sum>=10){
-        sum_node->val = sum%10;
-        up = (sum - sum%10)/10;
-      }
-      else{
-        sum_node->val = sum;
-        up=0;
-      }
-
-      pnode->next = sum_node;
-      pnode = sum_node;
-      if(tmp1 != NULL)
-        tmp1 = tmp1->next;
-      if(tmp2 != NULL)
-        tmp2 = tmp2->next;
-      if(tmp1 == NULL && tmp2 == NULL && up!=0){
-        sum_node = malloc(sizeof(struct ListNode));
-        sum_node->val = up;
-        pnode->next = sum_node;
-      }
-    }
-
-    //remove the header of result list
-    result = result->next;
-    return result;
-  }
+		    while(l1->next!=NULL||l2->next!=NULL){
+		        if(l1->next!=NULL&&l2->next==NULL){
+		            sum += l1->next->val;
+		            l1 = l1->next;
+		        }
+		        else if(l1->next==NULL&&l2->next!=NULL){
+		            sum += l2->next->val;
+		            l2 = l2->next;
+		        }
+		        else if(l1->next!=NULL&&l2->next!=NULL){
+		            sum += l1->next->val + l2->next->val;
+		            l1 = l1->next; l2 = l2->next;
+		        }
+		        if(sum>=10){
+		           ListNode *rnode = new ListNode(sum%10);
+		            sum /= 10;
+		            rpoint->next = rnode;
+		            rpoint = rpoint->next;
+		        }else{
+		            ListNode *rnode = new ListNode(sum);
+		            sum /= 10;
+		            rpoint->next = rnode;
+		            rpoint = rpoint->next;
+		        }
+		    }
+		    if(sum!=0){
+		        ListNode *rnode = new ListNode(sum);
+		        rpoint->next = rnode;
+		    }
+			return result;
+		}
 };
