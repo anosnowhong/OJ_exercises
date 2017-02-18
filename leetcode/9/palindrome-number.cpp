1a56f020ab8a887f7136c210b3e266a652ba9df8
anosnowhong <anosnowhong@gmail.com>
2017-02-18 18:32:19 +0800

    only need to compare half of the integer

--- leetcode/9/palindrome-number.cpp
+++ leetcode/9/palindrome-number.cpp
@@ -1,3 +1,17 @@
+class Solution {//A BETTER VERSION
+public:
+  bool isPalindrome(int x) {
+    if(x<0||x!=0&&x%10==0) return false;
+    int sum = 0;
+    while(x>sum){ //if x is a palindrome number, in the progress of building number 'sum', sum won't larger than x.
+      //when at the middle of integer, x is equal to sum.
+      sum = sum*10 + x%10;
+      x = x/10;
+    }
+    return (x==sum||x==sum/10);
+  }
+};
+
 class Solution {
 public:
   bool isPalindrome(int x) {
