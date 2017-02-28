9af114a206df4ac29612eec706e405ba031da85e
anosnowhong <anosnowhong@gmail.com>
2017-02-28 17:15:00 +0800

    two lines version

--- leetcode/88/merge-sorted-array.cpp
+++ leetcode/88/merge-sorted-array.cpp
@@ -1,14 +1,8 @@
+//the sorted order is not specified in the question
 class Solution {
 public:
   void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-    for(int i=m-1,j=n-1, tar = m+n-1; j>=0; ){
-      if(nums1[i]<nums2[j]||i<0){
-        nums1[tar--] = nums2[j--];
-      }
-      else{
-        nums1[tar--] = nums1[i--];
-      }
-
-    }
+    for(int i=m-1,j=n-1,tar = m+n-1; j>=0; )
+      (nums1[i]<nums2[j]||i<0)?nums1[tar--] = nums2[j--]:nums1[tar--]=nums1[i--];
   }
 };
